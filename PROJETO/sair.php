f70864709e2c461f353887d6476db5ea4d595827
Guilherme <gui_decol@hotmail.com>
2015-05-31 15:04:11 -0300

    Revert "Simulador"
    This reverts commit 4295c137fb0f80119c0015c5c239b06ca79a8558.

--- PROJETO/sair.php
+++
@@ -1,6 +0,0 @@
-<?php include_once 'sessao_validar.php';?>
-<?php
-	session_start();
-	session_destroy();
-	header("Location: index.php");
-?>
--- PROJETO/sessao_validar.php
+++
@@ -1,6 +0,0 @@
-<?php
-  session_start();
-  if (!(isset($_SESSION['NOME_USUARIO']) AND isset($_SESSION['ID_USUARIO']))) {
-    header("location: index.php");
-  }
-?>
